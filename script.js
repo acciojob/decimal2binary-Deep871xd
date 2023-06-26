d92c1c48656bcf8aa2c39dcddda22df3d8775e34
Deep871xd <102525444+Deep871xd@users.noreply.github.com>
2023-06-26 10:14:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,8 @@
-	function decimalToBinary(decimal) {
-  // Check if the input is 0
-  if (decimal === 0) {
-    return '0';
-  }
-
-  let binary = '';
-
-  while (decimal > 0) {
-    // Append the least significant bit (remainder of division by 2)
-    binary = (decimal % 2) + binary;
-
-    // Update the decimal by dividing it by 2 (integer division)
-    decimal = Math.floor(decimal / 2);
-  }
-
-  return binary;
+function threeSum(arr, target) {
+	let target = [];
+	for(let a = 0; a < arr.length; a++){
+		target.push(arr[a].toString(2));
+	}
+	return target;
 }
-  
-
-
-module.exports = threeSum;
+module.exports= threeSum;
