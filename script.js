23862c603e4b07367e2e24a581a7f5e45405d5b9
Deep871xd <102525444+Deep871xd@users.noreply.github.com>
2023-06-26 10:37:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,6 @@
-function convertToBinary(x) {
-    let bin = 0;
-    let rem, i = 1, step = 1;
-    while (x != 0) {
-        rem = x % 2;
-    
-        x = parseInt(x / 2);
-        bin = bin + rem * i;
-        i = i * 10;
-    }
-    // console.log(`Binary: ${bin}`);
-}
+ function DecimalToBinary(num) {
+    return num.toString(2);
+ }
 
-// take input
-let number = prompt('Enter a decim al number: ');
-
-convertToBinary(number);
+ let result=(DecimalToBinary(num));
+console.log(result);
