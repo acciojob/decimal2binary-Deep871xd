558768b035a09a41999fbe6ef532071fb334f625
Deep871xd <102525444+Deep871xd@users.noreply.github.com>
2023-06-26 10:39:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
- function DecimalToBinary(num) {
-    return num.toString(2);
- }
+function decimalToBinary(decimalNumber) {
+  // Check if the input is 0
+  if (decimalNumber === 0) {
+    return '0';
+  }
 
- let result=(DecimalToBinary(num));
-console.log(result);
+  // Create an empty array to store the binary digits
+  const binaryArray = [];
+
+  // Convert the decimal number to binary
+  while (decimalNumber > 0) {
+    // Get the remainder of division by 2
+    const remainder = decimalNumber % 2;
+
+    // Add the remainder to the binary array
+    binaryArray.unshift(remainder);
+
+    // Update the decimal number by dividing it by 2
+    decimalNumber = Math.floor(decimalNumber / 2);
+  }
+
+  // Join the binary digits into a string and return it
+  return binaryArray.join('');
+}
